2c9aeb1b95aa48767d904f129dda37bd021393c4
Sergio Pacheco <ch_e_ch_o@hotmail.com>
2021-03-30 16:32:37 -0300

    upload files

---
+++ db/init.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE flaskprinters;
+use flaskprinters;
+
+CREATE TABLE impresoras(id int AUTO_INCREMENT, name VARCHAR(255), ip VARCHAR(255), ip_publica VARCHAR(255), boquilla VARCHAR(255),cama VARCHAR(255),filamento VARCHAR(255),color_filamento VARCHAR(255), estado VARCHAR(255),PRIMARY KEY (id));
+
+
